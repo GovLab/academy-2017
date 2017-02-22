6e091ad4dde0c32bbf4b01c798b4085046507f65
mocxd <mark.adkins.hastings@gmail.com>
2017-02-22 15:39:05 -0500

    fix script to wait for images to be fully loaded

--- source/js/bg-image-loader.js
+++ source/js/bg-image-loader.js
@@ -3,17 +3,23 @@ $(function () {
 
     // check the aspect ratio of each image to see whether to use contain or cover for background-size
     $('[bg-url]').each(function() {
+        var $bgElement = $(this);
         var size = 'cover';
         var url = $(this).attr('bg-url');
         var img = new Image();
-        img.src = url;
 
-        var ar = img.width / img.height;
-        if (ar > 1 + threshold) {
-            size = 'contain';
-        }
-        $(this).css( 'background-image',  'url(' + url + ')' );
-        $(this).css( 'background-size', size );
+        // wait for image to be fully loaded
+        $(img).on('load', function(){
+            var ar = img.width / img.height;
+            if (ar > 1 + threshold) {
+                size = 'contain';
+            }
+            $bgElement.css( 'background-image',  'url(' + url + ')' );
+            $bgElement.css( 'background-size', size );
+        });
+
+        // set image src after handler is set up, otherwise never gets called
+        img.src = url;
     });
 
 });
