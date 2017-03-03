03cad2417b17ce443619a7a5bca1fae9d84853e0
enabek <enabek@gmail.com>
2017-03-03 12:20:33 -0500

    Update 2017-03-03T17:20:29.823Z

--- js/pdf.js
+++ js/pdf.js
@@ -1,30 +1,35 @@
 $(document).ready(function() {
   $(".download-button").click(function() {
     var doc = new jsPDF();
+    // window.print()
 
     doc.fromHTML($('#pdf-title').get(0), 10, 5);
-    doc.fromHTML($('#pdf-start').get(0), 10, 22);
-    doc.fromHTML($('#pdf-end').get(0), 10, 28);
+    doc.fromHTML($('#pdf-start').get(0), 10, 21);
+    doc.fromHTML($('#pdf-end').get(0), 10, 27);
 
     // doc.text("DESCRIPTION", 10, 55);
-    doc.fromHTML($('#pdf-desc').get(0), 10, 36, {'width': 180});
+    doc.fromHTML($('#pdf-desc').get(0), 10, 35, {'width': 180});
 
     // doc.text("CLASSES", 10, 110);
     var classes = document.getElementsByClassName('pdf-class-title');
     // var classWeek = document.getElementsByClassName('pdf-class-week');
     var classDesc = document.getElementsByClassName('pdf-class-desc');
 
-    var classHeight = 85;
-    var weekDesc = 95;
+    var classHeight = 87;
+    var weekDesc = 97;
 
     for (var i = 0, len = classes.length; i < len; i++) {
       doc.fromHTML(classes[i], 10, classHeight, {'width': 180});
       doc.fromHTML(classDesc[i], 10, weekDesc, {'width': 180});
+      if(classHeight >= 130 || weekDesc >= 130) {
+        classHeight = -60;
+        weekDesc = -50;
+        doc.addPage();
+      }
       classHeight += 80;
       weekDesc += 80;
     }
 
     doc.save('syllabus.pdf')
   })
-
 })
