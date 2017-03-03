f8252ebb5b17d157d014fde5dd6b841d532137bd
enabek <enabek@gmail.com>
2017-03-03 15:31:30 -0500

    Update 2017-03-03T20:31:29.510Z

--- js/pdf.js
+++ js/pdf.js
@@ -1,22 +1,22 @@
 $(document).ready(function() {
   $(".download-button").click(function() {
     var doc = new jsPDF();
-    // window.print()
 
-    doc.fromHTML($('#pdf-title').get(0), 10, 5);
-    doc.fromHTML($('#pdf-start').get(0), 10, 21);
-    doc.fromHTML($('#pdf-end').get(0), 10, 27);
+    doc.addImage($('#gov-logo').get(0), 65, 10, 80, 10);
+    doc.addImage($('#gov-footer').get(0), 7, 275, 195, 18);
+
+    doc.fromHTML($('#pdf-title').get(0), 10, 13);
+    doc.fromHTML($('#pdf-start').get(0), 10, 29);
 
     // doc.text("DESCRIPTION", 10, 55);
-    doc.fromHTML($('#pdf-desc').get(0), 10, 35, {'width': 180});
+    doc.fromHTML($('#pdf-desc').get(0), 10, 40, {'width': 180});
 
     // doc.text("CLASSES", 10, 110);
     var classes = document.getElementsByClassName('pdf-class-title');
-    // var classWeek = document.getElementsByClassName('pdf-class-week');
     var classDesc = document.getElementsByClassName('pdf-class-desc');
 
-    var classHeight = 87;
-    var weekDesc = 97;
+    var classHeight = 93;
+    var weekDesc = 103;
 
     for (var i = 0, len = classes.length; i < len; i++) {
       doc.fromHTML(classes[i], 10, classHeight, {'width': 180});
@@ -25,6 +25,14 @@ $(document).ready(function() {
         classHeight = -60;
         weekDesc = -50;
         doc.addPage();
+        doc.addImage($('#gov-logo').get(0), 65, 10, 80, 10);
+        doc.addImage($('#gov-footer').get(0), 7, 275, 195, 18);
+      } else if (classHeight >= 230 || weekDesc >= 230) {
+        classHeight = -60;
+        weekDesc = -50;
+        doc.addPage();
+        doc.addImage($('#gov-logo').get(0), 65, 10, 80, 10);
+        doc.addImage($('#gov-footer').get(0), 7, 275, 195, 18);
       }
       classHeight += 80;
       weekDesc += 80;
